a675296180e9ae04f63e23dd09bdca102f4ddc7c
PichurinR <pichurin.roman@gmail.com>
2015-10-31 02:22:54 +0200

    Create Classes

---
+++ GraphicsEditor/GraphicsEditor/Painter.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace GraphicsEditor
+{
+    abstract class Painter
+    {
+        abstract public void StartDrawing();
+        abstract public void Drawing();
+        abstract public void StopDrawing();
+        abstract public void CanvasNull();
+    }
+}
---
+++ GraphicsEditor/GraphicsEditor/PainterLine.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Shapes;
+
+namespace GraphicsEditor
+{
+    class PainterLine:Painter
+    {
+        Canvas canvas;
+        Line line;
+        public PainterLine(Canvas canvas)
+        {
+            this.canvas = canvas;
+            this.line = new Line();
+        }
+        public override void StartDrawing(Point point)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Drawing(Point point)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void StopDrawing(Point point)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void CanvasNull()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
